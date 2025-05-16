93fe23cdc6c9a679f69089eab7e23b266194e5dc
klocke-io <niklas.klocke@sap.com>
2025-05-16 11:02:36 +0200

    trigger workflow

--- packages/kube-config/lib/constants.js
+++
@@ -1,8 +0,0 @@
-//
-// SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and Gardener contributors
-//
-// SPDX-License-Identifier: Apache-2.0
-//
-
-export const KUBERNETES_SERVICEACCOUNT_TOKEN_FILE = '/var/run/secrets/kubernetes.io/serviceaccount/token'
-export const KUBERNETES_SERVICEACCOUNT_CA_FILE = '/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'
